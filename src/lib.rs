e0aa4dc7a243d61191c0400c26fbcf7ef9790c7d
王浩宇 <wanghaoyu.ff@bytedance.com>
2023-08-04 10:31:42 +0800

    fix: 调整转发策略

--- src/lib.rs
+++ src/lib.rs
@@ -1,74 +1,36 @@
-use std::sync::Arc;
-
 use tokio::{
-    io::{self, AsyncReadExt, AsyncWriteExt},
-    net::{
-        tcp::{OwnedReadHalf, OwnedWriteHalf},
-        TcpStream,
-    },
-    sync::Mutex,
+    io::{self},
+    net::TcpStream,
 };
-use tracing_log::log::{debug, error, info};
+use tracing_log::log::{error, info};
 
 pub struct TCPProxy {
-    income_rx: Arc<Mutex<OwnedReadHalf>>,
-    income_tx: Arc<Mutex<OwnedWriteHalf>>,
+    upstream_addr: String,
 }
 
 impl TCPProxy {
-    pub fn new(income: TcpStream) -> Self {
-        let (income_rx, income_tx) = income.into_split();
-        Self {
-            income_rx: Arc::new(Mutex::new(income_rx)),
-            income_tx: Arc::new(Mutex::new(income_tx)),
-        }
+    pub fn new(upstream_addr: String) -> Self {
+        Self { upstream_addr }
     }
-    pub async fn run(&mut self, upstream_addr: &str) -> Result<(), io::Error> {
-        let upstream = TcpStream::connect(upstream_addr).await?;
+    pub async fn run(&mut self, income: TcpStream) -> Result<(), io::Error> {
+        let (mut income_rx, mut income_tx) = income.into_split();
+        let upstream = TcpStream::connect(&self.upstream_addr).await?;
         let (mut upstream_rx, mut upstream_tx) = upstream.into_split();
-        let mut income_rx = self.income_rx.clone().lock_owned().await;
-        let mut income_tx = self.income_tx.clone().lock_owned().await;
-        let mut upstream_buf = [0u8; 512];
-        let mut income_buf = [0u8; 512];
-        loop {
-            let read_from_upstream = upstream_rx.read(&mut upstream_buf);
-            let read_from_income = income_rx.read(&mut income_buf);
-            tokio::select! {
-                read_rst = read_from_upstream =>match read_rst{
-                    Ok(read_n)=>{
-                        debug!("read {read_n} bytes from upstream");
-                        if read_n == 0{
-                            info!("read from upstream end.");
-                            return Ok(())
-                        }
-                        match income_tx.write(&upstream_buf[..read_n]).await{
-                            Ok(write_n)=> debug!("write {write_n} bytes to income"),
-                            Err(err)=>error!("write to income error: {err:?}"),
-                        };
-                    }
-                    Err(err)=>{
-                        error!("read from upstream error: {err:?}");
-                        return Err(err)
-                    },
-                },
-                read_rst = read_from_income=>match read_rst{
-                    Ok(read_n)=>{
-                        debug!("read {read_n} bytes from income");
-                        if read_n == 0{
-                            info!("read from income end.");
-                            return Ok(())
-                        }
-                        match upstream_tx.write(&upstream_buf[..read_n]).await{
-                            Ok(write_n)=> debug!("write {write_n} bytes to upstream"),
-                            Err(err)=>error!("write to upstream error: {err:?}"),
-                        };
-                    }
-                    Err(err)=>{
-                        error!("read from income error: {err:?}");
-                        return Err(err)
-                    },
-                },
+        tokio::spawn(async move {
+            let up = io::copy(&mut income_rx, &mut upstream_tx).await;
+            match up {
+                Ok(sent_n) => info!("send {sent_n} bytes to upstream"),
+                Err(err) => error!("send to upstream error: {err:?}"),
             }
-        }
+        });
+
+        tokio::spawn(async move {
+            let down = io::copy(&mut upstream_rx, &mut income_tx).await;
+            match down {
+                Ok(sent_n) => info!("send {sent_n} bytes to income"),
+                Err(err) => error!("send to income error: {err:?}"),
+            }
+        });
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
+use clap::Parser;
 use proxy::*;
 use tokio::net::TcpListener;
 use tracing_log::log::debug;
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Config {
+    #[arg(short, long)]
+    upstream: String,
+    #[arg(short, long)]
+    bind: String,
+}
+
 #[tokio::main]
 async fn main() {
     env_logger::init();
-    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    let cfg = Config::parse();
+    let listener = TcpListener::bind(cfg.bind).await.unwrap();
     loop {
         let (stream, addr) = listener.accept().await.unwrap();
         debug!("accept from {addr:?}");
-        let mut proxy = TCPProxy::new(stream);
+        let mut proxy = TCPProxy::new(cfg.upstream.clone());
         tokio::spawn(async move {
-            let _ = proxy.run("nb.byted.org:80").await;
+            let _ = proxy.run(stream).await;
         });
     }
 }
