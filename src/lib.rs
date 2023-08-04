33d6f19bd81e8a828c73a65a720be47a0a76ef85
王浩宇 <wanghaoyu.ff@bytedance.com>
2023-08-04 14:01:33 +0800

    feat: 添加配置文件,并支持多个proxy

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use tokio::{
     io::{self},
     net::TcpStream,
@@ -5,30 +7,35 @@ use tokio::{
 use tracing_log::log::{error, info};
 
 pub struct TCPProxy {
+    name: Arc<String>,
     upstream_addr: String,
 }
 
 impl TCPProxy {
-    pub fn new(upstream_addr: String) -> Self {
-        Self { upstream_addr }
+    pub fn new(name: String, upstream_addr: String) -> Self {
+        Self {
+            name: Arc::new(name),
+            upstream_addr,
+        }
     }
     pub async fn run(&mut self, income: TcpStream) -> Result<(), io::Error> {
         let (mut income_rx, mut income_tx) = income.into_split();
         let upstream = TcpStream::connect(&self.upstream_addr).await?;
         let (mut upstream_rx, mut upstream_tx) = upstream.into_split();
+        let name = self.name.clone();
         tokio::spawn(async move {
             let up = io::copy(&mut income_rx, &mut upstream_tx).await;
             match up {
-                Ok(sent_n) => info!("send {sent_n} bytes to upstream"),
-                Err(err) => error!("send to upstream error: {err:?}"),
+                Ok(sent_n) => info!("{name} send {sent_n} bytes to upstream"),
+                Err(err) => error!("{name} send to upstream error: {err:?}"),
             }
         });
-
+        let name = self.name.clone();
         tokio::spawn(async move {
             let down = io::copy(&mut upstream_rx, &mut income_tx).await;
             match down {
-                Ok(sent_n) => info!("send {sent_n} bytes to income"),
-                Err(err) => error!("send to income error: {err:?}"),
+                Ok(sent_n) => info!("{name} send {sent_n} bytes to income"),
+                Err(err) => error!("{name} send to income error: {err:?}"),
             }
         });
         Ok(())
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,75 @@
+use serde_derive::{Deserialize, Serialize};
+use std::path::PathBuf;
+
 use clap::Parser;
 use proxy::*;
-use tokio::net::TcpListener;
-use tracing_log::log::debug;
+use tokio::{
+    net::TcpListener,
+    task::{JoinHandle, JoinSet},
+};
+use tracing_log::log::{debug, info};
+
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<PathBuf>,
+    #[arg(short, long)]
+    upstream: Option<String>,
+    #[arg(short, long)]
+    bind: Option<String>,
+}
+#[derive(Default, Serialize, Deserialize)]
 struct Config {
-    #[arg(short, long)]
-    upstream: String,
-    #[arg(short, long)]
+    proxies: Vec<Proxy>,
+}
+#[derive(Serialize, Deserialize)]
+struct Proxy {
+    name: String,
     bind: String,
+    upstream: String,
 }
 
 #[tokio::main]
 async fn main() {
     env_logger::init();
-    let cfg = Config::parse();
-    let listener = TcpListener::bind(cfg.bind).await.unwrap();
+    let args = Args::parse();
+    if args.config.is_none() && (args.bind.is_none() && args.upstream.is_none()) {
+        panic!("config or upstream not found");
+    }
+    match args.config {
+        Some(path) => {
+            run_with_config(path).await;
+        }
+        None => {
+            if args.bind.is_none() || args.upstream.is_none() {
+                panic!("bind address or upstream not found");
+            }
+            run("proxy", &args.bind.unwrap(), &args.upstream.unwrap()).await;
+        }
+    }
+}
+
+async fn run(name: &str, bind: &str, upstream: &str) {
+    let listener = TcpListener::bind(bind).await.unwrap();
+    info!("listen {name}({upstream}) on {bind}...");
     loop {
         let (stream, addr) = listener.accept().await.unwrap();
-        debug!("accept from {addr:?}");
-        let mut proxy = TCPProxy::new(cfg.upstream.clone());
+        debug!("accept {name} connection from {addr:?}");
+        let mut proxy = TCPProxy::new(name.to_string(), upstream.to_string());
         tokio::spawn(async move {
             let _ = proxy.run(stream).await;
         });
     }
+}
+
+async fn run_with_config(path: PathBuf) {
+    let cfg: Config = confy::load_path(path).unwrap();
+    let mut set = JoinSet::new();
+    for proxy in cfg.proxies {
+        set.spawn(async move {
+            run(&proxy.name, &proxy.bind, &proxy.upstream).await;
+        });
+    }
+    while set.join_next().await.is_some() {}
 }
